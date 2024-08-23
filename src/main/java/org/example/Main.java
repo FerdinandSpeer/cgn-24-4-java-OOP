9eeb17f6a5755593b8261c33f525fcfe51ccc6a1
ferdinandspeer <ferdinandspeer@gmail.com>
2024-08-23 10:21:06 +0200

    Session finished

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,17 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Person pommes = new Person("Müller", 35,"0123456789", true);
+        Person rotWeiss = new Person("MBauer", 20,"0124566789", true);
+
+        x
+
+        Person [] phonebook = {pommes, rotWeiss};
+
+    }
+
+    public static void addPerson (Person currywurst) {
+
     }
 }
