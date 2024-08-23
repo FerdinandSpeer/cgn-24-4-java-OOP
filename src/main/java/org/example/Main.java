fb3aa852f57334220b34c4df8d326fe8c570b2ea
ferdinandspeer <ferdinandspeer@gmail.com>
2024-08-23 11:30:36 +0200

    basic challenge finished

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,18 @@ public class Main {
 
         Person pommes = new Person("Müller", 35,"0123456789", true);
         Person rotWeiss = new Person("MBauer", 20,"0124566789", true);
-
-        x
+        Car vw = new Car ("VW", "Golf", "black", 2020);
+        Car ferrari = new Car ("Ferrari", "Enzo", "red", 2023);
+        vw.starts();
+        vw.accelerate();
+        ferrari.starts();
+        ferrari.accelerate();
 
         Person [] phonebook = {pommes, rotWeiss};
-
     }
-
     public static void addPerson (Person currywurst) {
-
+        System.out.println();
     }
+
+
 }
